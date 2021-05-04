ff5c01236aa68ac69b5844bb5f4816f9483303ad
slondono19 <45316388+slondono19@users.noreply.github.com>
2021-05-04 14:04:05 -0700

    implemented the filter element

--- main.js
+++ main.js
@@ -3,11 +3,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 // Event listeners
 
 todoButton.addEventListener('click', addTodo);
-
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
+
 // Functions
 
 function addTodo(event){
@@ -20,7 +22,7 @@ function addTodo(event){
     //Create LI
 
     const newTodo = document.createElement('li')
-    newTodo.innerText = document.querySelector('.todo-input').value;
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
 
@@ -63,3 +65,27 @@ function deleteCheck(event){
         item.parentElement.classList.toggle("completed");
     }
 }
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+      switch (e.target.value) {
+        case "all":
+          todo.style.display = "flex";
+          break;
+        case "completed":
+          if (todo.classList.contains("completed")) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+          break;
+        case "uncompleted":
+          if (!todo.classList.contains("completed")) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+      }
+    });
+  }
